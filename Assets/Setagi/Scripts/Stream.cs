beb026746357b6542b5d59427df2e6348e72ecf7
setagiSan <118693438+SetagiSan@users.noreply.github.com>
2023-12-04 22:58:27 +0300

    InertsiFix

--- Assets/Setagi/Scripts/Stream.cs
+++ Assets/Setagi/Scripts/Stream.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class Stream : MonoBehaviour
 {
+    private enum vector {
+    forward = 0, backward = 1, up = 2, down = 3, right = 4, left = 5
+    };
     Vector3 speed;
     Rigidbody m_Rigidbody;
     Vector3 OLD_Position;
     public float m_Thrust = 20f;
+    [SerializeField] 
+    vector vectorFact;
     private void Start()
     {
+
         OLD_Position = transform.position;
     }
 
@@ -20,11 +26,60 @@ public class Stream : MonoBehaviour
     }
     private void OnTriggerStay(Collider other)
     {
-        print(other.gameObject.name);
-        if (other.gameObject.tag == "Boat"&& speed != new Vector3(0,0,0))
+        if (vectorFact == vector.forward)
         {
-            m_Rigidbody = other.GetComponent<Rigidbody>();
-            m_Rigidbody.AddForce(transform.forward * m_Thrust/10);
+            if (other.gameObject.tag == "Boat" && speed.z > 0.1)
+            {
+                m_Rigidbody = other.GetComponent<Rigidbody>();
+                m_Rigidbody.AddForce(transform.forward * speed.z / 2);
+            }
         }
+        if (vectorFact == vector.backward)
+        {
+            if (other.gameObject.tag == "Boat" && -speed.z >0.1)
+            {
+                m_Rigidbody = other.GetComponent<Rigidbody>();
+                m_Rigidbody.AddForce(transform.forward * speed.z / 2);
+            }
+
+        }
+        if (vectorFact == vector.up)
+        {
+            if (other.gameObject.tag == "Boat" && speed.y >0.1)
+            {
+                m_Rigidbody = other.GetComponent<Rigidbody>();
+                m_Rigidbody.AddForce(transform.up * speed.y / 2);
+            }
+
+        }
+        if (vectorFact == vector.down)
+        {
+            if (other.gameObject.tag == "Boat" && -speed.y > 0.1)
+            {
+                print(transform.up * speed.y / 2);
+                m_Rigidbody = other.GetComponent<Rigidbody>();
+                m_Rigidbody.AddForce(transform.up * speed.y / 2);
+            }
+
+        }
+        if (vectorFact == vector.right)
+        {
+            if (other.gameObject.tag == "Boat" && speed.x > 0.1)
+            {
+                m_Rigidbody = other.GetComponent<Rigidbody>();
+                m_Rigidbody.AddForce(transform.right * speed.x / 2);
+            }
+
+        }
+        if (vectorFact == vector.left)
+        {
+            if (other.gameObject.tag == "Boat" && -speed.x > 0.1)
+            {
+                m_Rigidbody = other.GetComponent<Rigidbody>();
+                m_Rigidbody.AddForce(transform.right * speed.x / 2);
+            }
+
+        }
+
     }
 }
