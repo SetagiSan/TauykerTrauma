ff0d2153e15e812805d85543f98a478f7ee2d6f9
setagiSan <118693438+SetagiSan@users.noreply.github.com>
2023-12-04 14:56:06 +0300

    SimpleStreamInersi

--- Assets/Setagi/Scripts/Stream.cs
+++ Assets/Setagi/Scripts/Stream.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Stream : MonoBehaviour
 {
+    Vector3 speed;
     Rigidbody m_Rigidbody;
+    Vector3 OLD_Position;
     public float m_Thrust = 20f;
+    private void Start()
+    {
+        OLD_Position = transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        speed = (transform.position - OLD_Position) / Time.deltaTime;
+        OLD_Position = transform.position;
+    }
     private void OnTriggerStay(Collider other)
     {
         print(other.gameObject.name);
-        if (other.gameObject.tag == "Boat")
+        if (other.gameObject.tag == "Boat"&& speed != new Vector3(0,0,0))
         {
             m_Rigidbody = other.GetComponent<Rigidbody>();
-            m_Rigidbody.AddForce(transform.up * m_Thrust);
+            m_Rigidbody.AddForce(transform.forward * m_Thrust/10);
         }
     }
 }
