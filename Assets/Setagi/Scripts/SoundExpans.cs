405866990aaa70080108ed3932935e76bc48b300
setagiSan <118693438+SetagiSan@users.noreply.github.com>
2023-12-05 14:06:45 +0300

    EcholotUpdate

---
+++ Assets/Setagi/Scripts/SoundExpans.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using UnityEngine;
+
+public class SoundExpans : MonoBehaviour
+{
+    [SerializeField] private float SoundDistance;
+    [SerializeField] private float SoundExpansSpeed;
+    [SerializeField] private float SoundExpansLifeTime;
+    public RaycastHit hit;
+    void Start()
+    {
+        transform.localScale = new Vector3(1,1,1);
+        Destroy(gameObject, SoundExpansLifeTime);
+    }
+
+    void FixedUpdate()
+    {
+        if (transform.localScale.magnitude <= SoundDistance)
+        {
+            transform.localScale *= SoundExpansSpeed;
+        }
+        else Destroy(gameObject);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Boat") {
+            Ray ray = new Ray(transform.position, other.transform.position-transform.position);
+            Physics.Raycast(ray, out hit);
+            if (hit.collider.tag == "Boat") {
+                print(hit.collider.gameObject);
+               // Debug.DrawRay(transform.position, other.transform.position - transform.position, Color.green);
+            }
+        }
+    }
+}
